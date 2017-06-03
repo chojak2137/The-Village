b447dd65591bc7b8dbf94adc02b1a17ae7353e5c
chojak2137 <chojecki2000@wp.pl>
2017-06-03 21:27:33 +0200

    #1
    blblblblblbl

---
+++ zamek/Assets/kilof.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kilof : MonoBehaviour {
+    private Animator anim;
+    public bool gora;
+
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetBool("Gora", true);
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetBool("Gora", false);
+        }
+    }
+}
